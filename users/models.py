19cfc989c14d4dc5a38f72388282ffe9c66f5665
Firthiyr <pomodorka05@gmail.com>
2024-05-28 19:44:09 +0300

    Start model user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+
+class Sizes(models.Model):
+    image = models.ImageField(upload_to="users_images", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "User"
+        verbose_name = "Користувач"
+        verbose_name_plural = "Користувачі"
