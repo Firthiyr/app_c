315286fc7a3b8e9274e9907596561229c9e147e0
Firthiyr <pomodorka05@gmail.com>
2024-06-01 20:45:04 +0300

    Add to cart

--- carts/urls.py
+++ carts/urls.py
@@ -4,11 +4,15 @@ from carts import views
 app_name = "carts"
 
 urlpatterns = [
-    path("add_to_cart/<int:product_id>", views.add_to_cart, name="add_to_cart"),
+    path("add_to_cart/<slug:product_slug>", views.add_to_cart, name="add_to_cart"),
     path(
-        "change_in_cart/<int:product_id>", views.change_in_cart, name="change_in_cart"
+        "change_in_cart/<slug:product_slug>",
+        views.change_in_cart,
+        name="change_in_cart",
     ),
     path(
-        "delete_in_cart/<int:product_id>", views.delete_in_cart, name="delete_in_cart"
+        "delete_in_cart/<slug:product_slug>",
+        views.delete_in_cart,
+        name="delete_in_cart",
     ),
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from goods.models import Products
+from carts.models import Cart
 
 
 # Create your views here.
-def add_to_cart(request, product_id): ...
+def add_to_cart(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user=request.user, product=product)
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(user=request.user, product=product, quantity=1)
+
+    return redirect(request.META["HTTP_REFERER"])
 
 
-def change_in_cart(request, product_id): ...
+def change_in_cart(request, product_): ...
 
 
-def delete_in_cart(request, product_id): ...
+def delete_in_cart(request, product_): ...
